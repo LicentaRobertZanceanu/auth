e5fd0d287688449aed2cf0e150a06e08624eca72
Robert Zanceanu <robert@sprybase.com>
2021-05-02 12:44:58 +0300

    Changed login response's expires from seconds to timestamp

--- utils/generate-login-tokens.js
+++ utils/generate-login-tokens.js
@@ -1,9 +1,10 @@
 import { generateToken } from "./generate-token.js"
+import moment from 'moment'
 
 export const generateLoginTokens = (user) => {
     return {
         authToken: generateToken(user, '3600s'),
         refreshToken: generateToken(user, '25200s'),
-        expires: '3600'
+        expires: moment().add(1, 'h').unix()
     }
 }
